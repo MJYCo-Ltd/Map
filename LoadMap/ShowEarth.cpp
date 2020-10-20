5a22ad4d7674f137b4c46619469719643263540c
ytyisme <wshyshb@163.com>
2020-10-20 08:18:57 +0800

    增加测试cover代码

---
+++ LoadMap/ShowEarth.cpp
@@ -0,0 +1,6 @@
+#include "ShowEarth.h"
+
+CShowEarth::CShowEarth(QWidget *parent) : QMainWindow(parent)
+{
+
+}
---
+++ LoadMap/ShowEarth.h
@@ -0,0 +1,16 @@
+#ifndef CSHOWEARTH_H
+#define CSHOWEARTH_H
+
+#include <QMainWindow>
+
+class CShowEarth : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit CShowEarth(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // CSHOWEARTH_H
