de94c63d2e691ee430d14014fca7eeca722de8c3
ytyisme <wshyshb@gmail.com>
2021-03-20 17:12:05 +0800

    提交自动构建

---
+++ AutoMake.bat
@@ -0,0 +1,13 @@
+setlocal
+call "C:\Qt\5.15.2\msvc2019_64\bin\qtenv2.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+cd /d %~dp0
+qmake
+nmake all 
+echo.
+echo.
+echo "====================重新编译成功===================="
+echo.
+echo.
+endlocal
+Pause
