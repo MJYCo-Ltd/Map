85a8ad1d91261077fcab3e24dd8db55cbe670902
CLH <262160448@qq.com>
2022-05-17 15:36:37 +0800

    同步

---
+++ Inc/Plot/Map/IMapGroup.h
@@ -0,0 +1,31 @@
+﻿#ifndef INTERFACE_MAP_GROUP_HEARDER_H
+#define INTERFACE_MAP_GROUP_HEARDER_H
+
+#include <Plot/Map/IMapSceneNode.h>
+/**
+ * @brief 地图位置定位模型
+ */
+class IMapGroup:public IMapSceneNode
+{
+public:
+
+    CONSTRUCTOR(IMapGroup,IMapSceneNode)
+
+    /**
+     * @brief 设置由SceneShpae创建的节点
+     * @param pNode
+     */
+    virtual void AddSceneNode(ISceneNode* pNode)=0;
+    virtual void RemoveSceneNode(ISceneNode* pNode)=0;
+    virtual ISceneNode* SceneNode(){return(m_pSceneNode);}
+
+    virtual void Update() =0;
+protected:
+    virtual ~IMapGroup(){}
+
+
+protected:
+    ISceneNode* m_pSceneNode{};
+};
+
+#endif//INTERFACE_MAP_LOCATION_HEARDER_H
